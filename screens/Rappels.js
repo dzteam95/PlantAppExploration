3141b4b37638a75aa44225e5b1ec20d43dc88ecd
Felix <57390705+Felik6@users.noreply.github.com>
2021-07-06 14:24:18 +0200

    Delete reminder plant in garden n garden element eability

--- screens/Rappels.js
+++ screens/Rappels.js
@@ -1,4 +1,5 @@
-import React, { useState,useEffect } from 'react';
+import React, {useState, useEffect } from 'react';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
 import {
     StyleSheet,
     Text,
@@ -15,6 +16,7 @@ import {
     setState,
     SectionList,
     AsyncStorage,
+    Animated,
 } from 'react-native';
 import {COLORS} from "../constants";
 import {Boarding1} from "../constants/images";
@@ -48,11 +50,13 @@ const Rappels = ({route, navigation,  props }) => {
         {id:6, day:'samedi'},
     ];
     const currentDayName = weekdays[currentDay.getDay()];
+    const [itemTodelete, setItemToDelete] = useState([]);
+
         // console.log(currentDayName);
 
-    useEffect(() => {
+    useEffect(async() => {
         // getParsedDate(currentDay)
-        searchUserReminderFunction()
+        await searchUserReminderFunction()
         return /*() => {
             readToken()
         }*/
@@ -114,11 +118,62 @@ const Rappels = ({route, navigation,  props }) => {
 
     }
 
-    eventClickListener = (viewId) => {
+    eventClickListenerReminder = (viewId) => {
         Alert.alert("Info", "Passez par une fiche plante pour ajouter un rappel !");
         console.log('Date:'+currentDay ,);
     }
 
+    const leftSwipe = (progress, dragX) => {
+        const scale = dragX.interpolate({
+            inputRange: [0,100],
+            outputRange: [0,1],
+            extrapolate: 'clamp',
+        })
+        return (
+            // <TouchableOpacity onPress={this.handleDelete} activeOpacity={0,6}>
+            <View style={styles.eventBoxTD} >
+                <TouchableOpacity style={styles.eventContentTD} onPress={deleteItem(itemTodelete)} activeOpacity={0,6}>
+                {/* <TouchableOpacity onPress={deleteItem()} activeOpacity={0,6}> */}
+                    <View style={styles.deleteBox}>
+                        <Animated.Text>X</Animated.Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        )
+
+    }
+    // handleDelete=(id)=> deleteItem(id)
+
+    const deleteItem = async (id) => {
+        console.log(id)
+        const token = await AsyncStorage.getItem('id_token')
+        console.log(token);
+
+        let dataDelete = {
+            method: 'DELETE',
+            credentials: 'same-origin',
+            mode: 'same-origin',
+            headers: {
+                'Accept': '*/*',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer '+token,
+                // 'Authorization': 'Bearer '+tokenLocal,
+            },
+        }
+
+        fetch(`https://seedyapp.tk/reminder/${id}`, dataDelete)
+            .then( res => {
+                console.log(res.status)
+                if (res.status === 200) {
+                    console.log('garden deleted'); 
+                    navigation.replace('Rappels')                       
+                }else{
+                    console.log('not authorized');
+                }
+            })
+    }
+
+    // console.log(result[0].id_plant)
     return (
         <View style={styles.container}>
 
@@ -127,7 +182,7 @@ const Rappels = ({route, navigation,  props }) => {
                         <Text style={styles.name}>
                            Rappels
                         </Text>
-                        <TouchableOpacity onPress={() => this.eventClickListener("row")}>
+                        <TouchableOpacity onPress={() => this.eventClickListenerReminder("row")}>
                             <Text style={styles.add}>
                             +
                             </Text>
@@ -141,79 +196,95 @@ const Rappels = ({route, navigation,  props }) => {
                         /> */}
                     </View>
                 </View>
-                <SectionList
-                        style={styles.showCase}
-                        sections={[
-                            {title: "", data: result},                            
-                        ]}
-                        showsVerticalScrollIndicator={false}
-                        renderItem={({item}) => 
-                            // <View>
-                                <View style={styles.eventBox}>
-                                    <View style={styles.eventContent}>
-                                        <View style={styles.eventContentFirst}>
-                                            <Image style={styles.tinyLogo} source={{ uri: item.plantImg,}}/>
-                                            <Text  style={styles.plante}>{item.plantName} | {item.work}</Text>
-                                            {/* <Image style={styles.tinyLogo} source={{ uri: item.imgwork,}}/> */}
-                                        </View>
-                                        <View style={styles.eventContentSec}>
-                                            <Text  style={styles.eventTime}>Le {item.actionDate}</Text>
-                                        </View>
-                                        <View style={styles.eventContentSec}>
-                                            <Text  style={styles.description}>Vous devez : {item.title}</Text>
-                                        </View>
+                {/* <ScrollView> */}
+                    <FlatList
+                            style={styles.showCase}
+                            // sections={[
+                            //     {title: "", data: result},                            
+                            // ]}
+                            data={result}
+                            showsVerticalScrollIndicator={false}
+                            renderItem={({item}) => 
+                                // <View>
+                                    <View style={styles.eventBox}>
+                                        <TouchableOpacity style={styles.eventContent}>
+                                            <Swipeable style={styles.eventContentSwip} renderLeftActions={leftSwipe} onSwipeableLeftOpen={() =>setItemToDelete(item.id)}>
+
+                                                <View style={styles.eventContentFirst}>
+                                                    <Image style={styles.tinyLogo} source={{ uri: item.plantImg,}}/>
+                                                    <Text  style={styles.plante}>{item.plantName} | {item.work}</Text>
+                                                    {/* <Image style={styles.tinyLogo} source={{ uri: item.imgwork,}}/> */}
+                                                </View>
+                                                <View style={styles.eventContentSec}>
+                                                    <Text  style={styles.eventTime}>Le {item.actionDate}</Text>
+                                                </View>
+                                                <View style={styles.eventContentSec}>
+                                                    <Text  style={styles.description}>Vous devez : {item.title}</Text>
+                                                </View>
+                                                {/* <View style={styles.eventContent}> */}
+                                                {/* <TouchableOpacity
+                                                    style={styles.eventContent}
+                                                    onPress={() =>
+                                                        navigation.navigate('ConseilsDetail', {
+                                                            item: item.id_plant,
+                                                            tokenPass: token,
+                                                            itemlink: 'plants'
+                                                        })
+                                                    }> */}
+                                                        {/* <Text  style={styles.plante}>{item.plantName} | {item.work}</Text> */}
+                                                        {/* <Text  style={styles.plante}>{item.id_plant} | {item.work}</Text> */}
+                                                {/* </TouchableOpacity> */}
+                                                {/* </View> */}
+                                            </Swipeable>
+                                        </TouchableOpacity>
                                     </View>
-                                </View>
-                            // </View>
-                        }
-                        renderSectionHeader={({section}) => 
-                            <Text style={styles.sectionHeader}>{section.title}</Text>
-                        }
-                        keyExtractor={(item, index) => index}
-                    />
-                {/* <FlatList
-                    enableEmptySections={true}
-                    style={styles.eventList}
-                    data={weekdays}
-                    keyExtractor= {(item) => {
-                        return item.id;
-                    }}
-                    renderItem={({item}) => {
-                        return (
-                            <TouchableOpacity >
-                                <View style={styles.eventDate}>
-                                    <Text  style={styles.eventDay}>{item.day}</Text>
-                                </View>    
-                                <FlatList
-                                    enableEmptySections={true}
-                                    style={styles.eventList}
-                                    data={result}
-                                    keyExtractor= {(item) => {
-                                        return item.id;
-                                    }}
-                                    renderItem={({item}) => {
-                                        return (
-                                            <TouchableOpacity onPress={() => this.eventClickListener("row")}>
-                                                <View style={styles.eventBox}>
-                                                    <View style={styles.eventContent}>
-                                                        <View style={styles.eventContentFirst}>
-                                                            <Image style={styles.tinyLogo} source={{ uri: item.imghref,}}/>
-                                                            <Text  style={styles.plante}>{item.plant} | {item.work}</Text>
-                                                            <Image style={styles.tinyLogo} source={{ uri: item.imgwork,}}/>
-                                                        </View>
-                                                        <View style={styles.eventContentSec}>
-                                                            <Text  style={styles.eventTime}>10:00 am - 10:45 am</Text>
-                                                            
-                                                            <Text  style={styles.description}>Lorem ipsum dolor sit amet, elit consectetur</Text>
+                                // </View>
+                            }
+                        />
+                    {/* <FlatList
+                        enableEmptySections={true}
+                        style={styles.eventList}
+                        data={weekdays}
+                        keyExtractor= {(item) => {
+                            return item.id;
+                        }}
+                        renderItem={({item}) => {
+                            return (
+                                <TouchableOpacity >
+                                    <View style={styles.eventDate}>
+                                        <Text  style={styles.eventDay}>{item.day}</Text>
+                                    </View>    
+                                    <FlatList
+                                        enableEmptySections={true}
+                                        style={styles.eventList}
+                                        data={result}
+                                        keyExtractor= {(item) => {
+                                            return item.id;
+                                        }}
+                                        renderItem={({item}) => {
+                                            return (
+                                                <TouchableOpacity onPress={() => this.eventClickListener("row")}>
+                                                    <View style={styles.eventBox}>
+                                                        <View style={styles.eventContent}>
+                                                            <View style={styles.eventContentFirst}>
+                                                                <Image style={styles.tinyLogo} source={{ uri: item.imghref,}}/>
+                                                                <Text  style={styles.plante}>{item.plant} | {item.work}</Text>
+                                                                <Image style={styles.tinyLogo} source={{ uri: item.imgwork,}}/>
+                                                            </View>
+                                                            <View style={styles.eventContentSec}>
+                                                                <Text  style={styles.eventTime}>10:00 am - 10:45 am</Text>
+                                                                
+                                                                <Text  style={styles.description}>Lorem ipsum dolor sit amet, elit consectetur</Text>
+                                                            </View>
                                                         </View>
                                                     </View>
-                                                </View>
-                                            </TouchableOpacity>
-                                        )
-                                }}/>
-                            </TouchableOpacity>
-                        )
-                    }}/> */}
+                                                </TouchableOpacity>
+                                            )
+                                    }}/>
+                                </TouchableOpacity>
+                            )
+                        }}/> */}
+                    {/* </ScrollView> */}
             </View>
     );
 }
@@ -256,13 +327,23 @@ const styles = StyleSheet.create({
     },
     eventContent: {
         flex:1,
-        flexDirection: 'column',
-        alignItems: 'flex-start',
-        marginLeft:10,
-        marginRight:10,
+        // flexDirection: 'column',
+        // alignItems: 'flex-start',
+        // // marginLeft:10,
+        // // marginRight:10,
         backgroundColor: '#FFFFFF',
         padding:10,
         borderRadius:10,
+    },
+    eventContentSwip: {
+        flex:1,
+        // flexDirection: 'column',
+        // alignItems: 'flex-start',
+        // marginLeft:10,
+        // marginRight:10,
+        backgroundColor: '#AA0000',
+        // padding:10,
+        // borderRadius:10,
     },
     eventContentFirst: {
         flex:1,
@@ -312,4 +393,4 @@ const styles = StyleSheet.create({
 });
 
 
-export default Rappels
+export default Rappels;
